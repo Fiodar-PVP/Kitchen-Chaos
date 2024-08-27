1ea5b92fdce5019e92deaab8ecd7233af4d45520
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-08-27 10:52:13 +0900

    Update ClearCounter
    Make use of the KitchenObjectSO to spawn KitchenObjects on Counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,11 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact()
     {
         Debug.Log("Interact!");
+
+        Transform kitchenObjectSOTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectSOTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectSOTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
     }
 }

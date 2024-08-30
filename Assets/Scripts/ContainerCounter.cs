50823ce1691d293f91e0458c62d54d24a2e67315
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-08-30 17:12:58 +0900

    Add Container Counter script
    It only spawns object and immediately gives it to player

---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class ContainerCounter : BaseCounter
+{
+    public event EventHandler OnPlayerGrabbedObject;
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    /// <summary>
+    /// Handles interaction logic between the player and the Container Counter.
+    /// If the kitchen object is not present, it instantiates one and gives it to the player.
+    /// </summary>
+    /// <param name="player">The Player interacting with the kitchen object.</param>
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            Transform kitchenObjectSOTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectSOTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}

048a5872c5aaa5b0055f8bc381f02aec47d90063
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-09-02 13:50:03 +0900

    Add CuttingCounter Script

---
+++ Assets/Scripts/CuttingCounter.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class CuttingCounter : BaseCounter
+{
+    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+
+    /// <summary>
+    /// Handles the interaction between the player and the kitchen counter. 
+    /// This method checks whether there is a kitchen object on the counter 
+    /// and whether the player is holding a kitchen object, and acts accordingly.
+    /// </summary>
+    /// <param name="player">The player interacting with the kitchen counter.</param>
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            //There is no kitchen object on counter
+            if (player.HasKitchenObject())
+                //Player has a kitchen object. Put it on counter.
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            else
+            {
+                //Player does not have a kitchen object
+            }
+        }
+        else
+        {
+            //There is a kitchen object on counter
+            if (player.HasKitchenObject())
+            {
+                //Player has a kitchen object
+            }
+            else
+            {
+                //Player does not have a kitchen object. Give him a kitchen object.
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+
+    public override void InteractAlternate(Player player)
+    {
+        if(HasKitchenObject())
+        {
+            //There is a kitchen object on counter
+            GetKitchenObject().DestroySelf();
+
+            KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+        }
+    }
+}

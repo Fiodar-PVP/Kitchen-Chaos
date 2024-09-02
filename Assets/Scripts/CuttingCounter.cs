916f9c4678d292bb4e9ba29f08f7eba7105f4e95
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-09-02 14:32:44 +0900

    Make use of CuttingRecipeSO in CuttingCounter logic

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
 
     /// <summary>
     /// Handles the interaction between the player and the kitchen counter. 
@@ -16,8 +16,14 @@ public class CuttingCounter : BaseCounter
         {
             //There is no kitchen object on counter
             if (player.HasKitchenObject())
-                //Player has a kitchen object. Put it on counter.
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+            {
+                //Player has a kitchen object
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    //Player has a kitchen object which can be cut
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
+            }
             else
             {
                 //Player does not have a kitchen object
@@ -38,14 +44,44 @@ public class CuttingCounter : BaseCounter
         }
     }
 
+    /// <summary>
+    /// Handles the alternate interaction between the player and the kitchen counter. 
+    /// This method checks whether there is a kitchen object on the counter 
+    /// and whether there is a cut recipe for it, and acts accordingly.
+    /// </summary>
+    /// <param name="player">The player interacting with the kitchen counter.</param>
     public override void InteractAlternate(Player player)
     {
-        if(HasKitchenObject())
+        if(HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO()))
         {
-            //There is a kitchen object on counter
+            //There is a kitchen object on counter AND it can be cut
+            KitchenObjectSO kitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
+
             GetKitchenObject().DestroySelf();
 
-            KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+            KitchenObject.SpawnKitchenObject(kitchenObjectSO, this);
         }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO kitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == kitchenObjectSO)
+                return true;
+        }
+
+        return false;
+    }
+
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO kitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == kitchenObjectSO)
+                return cuttingRecipeSO.output;
+        }
+
+        return null;
     }
 }

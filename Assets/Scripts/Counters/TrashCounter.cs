bb4134750767f99a5f7a3b2135c11a247142905f
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-09-03 14:59:46 +0900

    Create TrashCounter script and Prefab

---
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,17 @@
+public class TrashCounter : BaseCounter
+{
+    /// <summary>
+    /// Handles the interaction between the player and the trash counter. 
+    /// This method checks whether player has a kitchen object and if so
+    /// destroys it.
+    /// </summary>
+    /// <param name="player">The player interacting with the kitchen counter.</param>
+    public override void Interact(Player player)
+    {
+        if(player.HasKitchenObject())
+        {
+            //Player has a kitchen object;
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}

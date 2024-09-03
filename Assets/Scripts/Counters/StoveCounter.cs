a94be574d23ad953e9c24ae7606ab443664c1ec6
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-09-03 17:09:04 +0900

    Create StoveCounter prefab and script
    Use simple state machine to control cooking process on stove Counter

---
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -0,0 +1,178 @@
+using System;
+using UnityEngine;
+
+public class StoveCounter : BaseCounter
+{
+    public event EventHandler<OnStateChangedEventArgs> OnStateChanged;
+    public class OnStateChangedEventArgs : EventArgs
+    {
+        public State state;
+    }
+
+    [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
+    [SerializeField] private BurningRecipeSO[] burningRecipeSOArray;
+
+    public enum State
+    {
+        Idle,
+        Frying,
+        Fried,
+        Burned,
+    }
+
+    private State state = State.Idle;
+
+    private float fryingTimer;
+    private FryingRecipeSO fryingRecipeSO;
+
+    private float burningTimer;
+    private BurningRecipeSO burningRecipeSO;
+
+    private void Start()
+    {
+        state = State.Idle;
+    }
+
+    private void Update()
+    {
+        if(HasKitchenObject())
+        {
+            switch (state)
+            {
+                case State.Idle:
+                    break;
+
+                case State.Frying:
+
+                    fryingTimer += Time.deltaTime;
+
+                    if (fryingTimer >= fryingRecipeSO.fryingTimerMax)
+                    {
+                        //Fried
+                        GetKitchenObject().DestroySelf();
+
+                        KitchenObject.SpawnKitchenObject(fryingRecipeSO.output, this);
+
+                        state = State.Fried;
+
+                        burningTimer = 0f;
+
+                        burningRecipeSO = GetBurningRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+
+                        OnStateChanged?.Invoke(this, new OnStateChangedEventArgs { state = state });
+                    }
+                    break;
+
+                case State.Fried:
+
+                    burningTimer += Time.deltaTime;
+
+                    
+
+                    if (burningTimer >= burningRecipeSO.fryingTimerMax)
+                    {
+                        //Burned
+                        GetKitchenObject().DestroySelf();
+
+                        KitchenObject.SpawnKitchenObject(burningRecipeSO.output, this);
+
+                        state = State.Burned;
+
+                        OnStateChanged?.Invoke(this, new OnStateChangedEventArgs { state = state });
+                    }
+                    break;
+
+                case State.Burned:
+                    break;
+            }
+        }
+
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            //There is no kitchen object on counter
+            if (player.HasKitchenObject())
+            {
+                //Player has a kitchen object
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    //Player has a kitchen object which can be cut
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+
+                    fryingRecipeSO = GetFryingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+
+                    state = State.Frying;
+
+                    fryingTimer = 0f;
+
+                    OnStateChanged?.Invoke(this, new OnStateChangedEventArgs { state = state });
+                }
+            }
+            else
+            {
+                //Player does not have a kitchen object
+            }
+        }
+        else
+        {
+            //There is a kitchen object on counter
+            if (player.HasKitchenObject())
+            {
+                //Player has a kitchen object
+            }
+            else
+            {
+                //Player does not have a kitchen object. Give him a kitchen object.
+                GetKitchenObject().SetKitchenObjectParent(player);
+
+                state = State.Idle;
+
+                OnStateChanged?.Invoke(this, new OnStateChangedEventArgs { state = state });
+            }
+        }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO kitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(kitchenObjectSO);
+        return fryingRecipeSO != null;
+    }
+
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO kitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(kitchenObjectSO);
+        return fryingRecipeSO.output;
+    }
+
+    /// <summary>
+    /// Retrieves the FryingRecipeSO that corresponds to a given input kitchen object.
+    /// </summary>
+    /// <param name="inputKitchenObjectSO">The KitchenObjectSO to match against the input in the recipe.</param>
+    /// <returns>
+    /// The FryingRecipeSO that has the matching input kitchen object, or null if no match is found.
+    /// </returns>
+    private FryingRecipeSO GetFryingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (FryingRecipeSO fryingRecipeSO in fryingRecipeSOArray)
+        {
+            if (fryingRecipeSO.input == inputKitchenObjectSO)
+                return fryingRecipeSO;
+        }
+
+        return null;
+    }
+
+    private BurningRecipeSO GetBurningRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (BurningRecipeSO burningRecipeSO in burningRecipeSOArray)
+        {
+            if (burningRecipeSO.input == inputKitchenObjectSO)
+                return burningRecipeSO;
+        }
+
+        return null;
+    }
+}

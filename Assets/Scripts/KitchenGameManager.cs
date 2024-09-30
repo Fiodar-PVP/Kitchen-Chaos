b4011ccdfb221e551fde9a97807f129ee3024c7f
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-09-30 10:13:12 +0900

    Create KitchenGameManager to handle different state of game

---
+++ Assets/Scripts/KitchenGameManager.cs
@@ -0,0 +1,79 @@
+using System;
+using UnityEngine;
+
+public class KitchenGameManager : MonoBehaviour
+{
+    public static KitchenGameManager Instance { get; private set; }
+
+    public event EventHandler OnStateChanged;
+
+    private enum State
+    {
+        WaitingToStart,
+        CountdownToStart,
+        GamePlaying,
+        GameOver,
+    }
+
+    private float waitingToStartTimer = 1.0f;
+    private float countdownToStartTimer = 3.0f;
+    private float GamePlayingTimer = 10.0f;
+
+    private State state;
+
+    private void Awake()
+    {
+        state = State.WaitingToStart;
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.WaitingToStart:
+                waitingToStartTimer -= Time.deltaTime;
+                if(waitingToStartTimer < 0)
+                {
+                    state = State.CountdownToStart;
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+                break;
+            case State.CountdownToStart:
+                countdownToStartTimer -= Time.deltaTime;
+                if (countdownToStartTimer < 0)
+                {
+                    state = State.GamePlaying;
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+                break;
+            case State.GamePlaying:
+                GamePlayingTimer -= Time.deltaTime;
+                if (GamePlayingTimer < 0)
+                {
+                    state = State.GameOver;
+                    OnStateChanged?.Invoke(this, EventArgs.Empty);
+                }
+                break;
+            case State.GameOver:
+                break;
+        }
+
+        Debug.Log(state);
+    }
+
+    public bool IsPlaying()
+    {
+        return state == State.GamePlaying;
+    }
+
+    public bool IsCountdownToStartActive()
+    {
+        return state == State.CountdownToStart;
+    }
+
+    public float GetCountdownToStartTimer()
+    {
+        return countdownToStartTimer;
+    }
+}

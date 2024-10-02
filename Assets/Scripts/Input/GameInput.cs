ea81c46ca95a25ded1d64dfb70e322e92112a786
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-10-02 15:20:08 +0900

    Add Runtime Rebind Key functionality to Option UI

--- Assets/Scripts/Input/GameInput.cs
+++ Assets/Scripts/Input/GameInput.cs
@@ -1,13 +1,26 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+    private const string PLAYER_PREFS_BINDINGS = "INPUT_BINDINGS";
     public static GameInput Instance { get; private set; }
 
     public event EventHandler OnInteractAction;
     public event EventHandler OnInteractAlternateAction;
     public event EventHandler OnPauseAction;
+
+    public enum Binding
+    {
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight,
+        Interact,
+        InteractAlternate,
+        Pause
+    }
 
     private PlayerInputActions playerInputActions;
 
@@ -16,6 +29,11 @@ public class GameInput : MonoBehaviour
         Instance = this;
 
         playerInputActions = new PlayerInputActions();
+        if(PlayerPrefs.HasKey(PLAYER_PREFS_BINDINGS))
+        {
+            playerInputActions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_PREFS_BINDINGS));
+        }
+
         playerInputActions.Player.Enable();
 
         playerInputActions.Player.Interact.performed += Interact_performed;
@@ -32,17 +50,17 @@ public class GameInput : MonoBehaviour
         playerInputActions.Dispose();
     }
 
-    private void Pause_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    private void Pause_performed(InputAction.CallbackContext obj)
     {
         OnPauseAction?.Invoke(this, EventArgs.Empty);
     }
 
-    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    private void Interact_performed(InputAction.CallbackContext obj)
     {
         OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
 
-    private void InteractAlternate_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    private void InteractAlternate_performed(InputAction.CallbackContext obj)
     {
         OnInteractAlternateAction?.Invoke(this, EventArgs.Empty);
     }
@@ -61,5 +79,97 @@ public class GameInput : MonoBehaviour
         inputVector = inputVector.normalized;
 
         return inputVector;
+    }
+
+    public void RebindBinding(Binding binding, Action OnActionRebound)
+    {
+        playerInputActions.Player.Disable();
+
+        InputAction inputAction;
+        int bindingIndex;
+
+        switch (binding)
+        {
+            default:
+            case Binding.MoveUp:
+                {
+                    inputAction = playerInputActions.Player.Move;
+                    bindingIndex = 1;
+                    break;
+                }
+            case Binding.MoveDown:
+                {
+                    inputAction = playerInputActions.Player.Move;
+                    bindingIndex = 2;
+                    break;
+                }
+            case Binding.MoveLeft:
+                {
+                    inputAction = playerInputActions.Player.Move;
+                    bindingIndex = 3;
+                    break;
+                }
+            case Binding.MoveRight:
+                {
+                    inputAction = playerInputActions.Player.Move;
+                    bindingIndex = 4;
+                    break;
+                }
+            case Binding.Interact:
+                {
+                    inputAction = playerInputActions.Player.Interact;
+                    bindingIndex = 0;
+                    break;
+                }
+            case Binding.InteractAlternate:
+                {
+                    inputAction = playerInputActions.Player.InteractAlternate;
+                    bindingIndex = 0;
+                    break;
+                }
+            case Binding.Pause:
+                {
+                    inputAction = playerInputActions.Player.Pause;
+                    bindingIndex = 0;
+                    break;
+                }
+
+            
+        }
+
+        inputAction.PerformInteractiveRebinding(bindingIndex)
+            .OnComplete(callback =>
+                {
+                    callback.Dispose();
+                    OnActionRebound();
+                    playerInputActions.Player.Enable();
+                    
+                    PlayerPrefs.SetString(PLAYER_PREFS_BINDINGS, playerInputActions.SaveBindingOverridesAsJson());
+                    PlayerPrefs.Save();
+                })
+            .Start();
+
+    }
+
+    public string GetBindingText(Binding binding)
+    {
+        switch (binding)
+        {
+            default:
+            case Binding.MoveUp:
+                return playerInputActions.Player.Move.bindings[1].ToDisplayString();
+            case Binding.MoveDown:
+                return playerInputActions.Player.Move.bindings[2].ToDisplayString();
+            case Binding.MoveLeft:
+                return playerInputActions.Player.Move.bindings[3].ToDisplayString();
+            case Binding.MoveRight:
+                return playerInputActions.Player.Move.bindings[4].ToDisplayString();
+            case Binding.Interact:
+                return playerInputActions.Player.Interact.bindings[0].ToDisplayString();
+            case Binding.InteractAlternate:
+                return playerInputActions.Player.InteractAlternate.bindings[0].ToDisplayString();
+            case Binding.Pause:
+                return playerInputActions.Player.Pause.bindings[0].ToDisplayString();
+        }
     }
 }

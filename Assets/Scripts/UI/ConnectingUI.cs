68f94852329fde044d75e165f03e47f9b21874a9
Fiodaru <f.perakhozhau@pvp.co.jp>
2024-10-17 15:18:25 +0900

    Create UIs in Test Lobby Scene to handle Connection Approval results

---
+++ Assets/Scripts/UI/ConnectingUI.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class ConnectingUI : MonoBehaviour
+{
+    private void Start()
+    {
+        KitchenGameMultiplayer.Instance.OnTryingToJoin += KitchenGameMultiplayer_OnTryingToJoin;
+        KitchenGameMultiplayer.Instance.OnFailedToJoin += KitchenGameMultiplayer_OnFailedToJoin;
+
+        Hide();
+    }
+
+    private void KitchenGameMultiplayer_OnFailedToJoin(object sender, System.EventArgs e)
+    {
+        Hide();
+    }
+
+    private void KitchenGameMultiplayer_OnTryingToJoin(object sender, System.EventArgs e)
+    {
+        Show();
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        KitchenGameMultiplayer.Instance.OnTryingToJoin -= KitchenGameMultiplayer_OnTryingToJoin;
+        KitchenGameMultiplayer.Instance.OnFailedToJoin -= KitchenGameMultiplayer_OnFailedToJoin;
+    }
+}
